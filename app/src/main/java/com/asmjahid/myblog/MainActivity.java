6f5bd92ed04a1737981ff6fb7d7b6764e9aba80a
MD JAHIDUL ALAM MOJUMDER <asmjahid@users.noreply.github.com>
2017-12-09 12:56:48 +0600

    Fixed bug.Not showing list view item on Recyleview.

--- app/src/main/java/com/asmjahid/myblog/MainActivity.java
+++ app/src/main/java/com/asmjahid/myblog/MainActivity.java
@@ -2,6 +2,7 @@ package com.asmjahid.myblog;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -28,21 +30,41 @@ public class MainActivity extends AppCompatActivity {
     public RecyclerView mBlogList;
     public DatabaseReference mDatabase;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser() == null) {
+
+                    Intent loginIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                    loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                }
+            }
+        };
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog_post");
 
         mBlogList = findViewById(R.id.blog_list);
-        mBlogList.setHasFixedSize(true);
+        mBlogList.setHasFixedSize(false);
         mBlogList.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+
         FirebaseRecyclerOptions<Blog> options =
                 new FirebaseRecyclerOptions.Builder<Blog>()
                         .setQuery(mDatabase, Blog.class)
@@ -61,12 +83,13 @@ public class MainActivity extends AppCompatActivity {
                 // Create a new instance of the ViewHolder, in this case we are using a custom
                 // layout called R.layout.blog_list for each item
                 View view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.activity_main, parent, false);
+                        .inflate(R.layout.blog_list, parent, false);
 
                 return new BlogViewHolder(view);
             }
         };
         mBlogList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
     }
 
     public static class BlogViewHolder extends RecyclerView.ViewHolder {
